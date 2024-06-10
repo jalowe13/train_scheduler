a0f27b1be76d045b68f21b152851bee9614037f7
jalowe13 <jplowe21@vt.edu>
2024-06-10 10:35:30 -0500

    Payload Output To Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ const App: React.FC = () => {
   const [isGraphQL, setIsGraphQL] = useState(true);
   // Message State
   const [messageApi, contextHolder] = message.useMessage();
+  // Data State
+  const [data, setData] = useState(null);
   const info = () => {
     messageApi.info("Request Submitted");
   };
@@ -23,6 +25,7 @@ const App: React.FC = () => {
       try {
         const data = await apiFunction(isGraphQL);
         console.log("Api data:", data);
+        setData(data); // Set the data state
         return data;
       } catch (error) {
         console.error(`Failed to fetch data`, error);
@@ -34,26 +37,37 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         {contextHolder}
-        <NewTrainServiceForm
-          handleButtonClick={handleButtonClick}
-          info={info}
-          isGraphQL={isGraphQL}
-        />
-        <RequestSchedule
-          handleButtonClick={handleButtonClick}
-          info={info}
-          isGraphQL={isGraphQL}
-        />
-        <h4>API Type</h4>
-        <Switch
-          checkedChildren="GraphQL"
-          unCheckedChildren="REST"
-          defaultChecked
-          onChange={(checked) => setIsGraphQL(checked)}
-        />
-        {
-          // TODO: Delete Database buttonz
-        }
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ marginTop: "100px", marginRight: "100px" }}>
+            <NewTrainServiceForm
+              handleButtonClick={handleButtonClick}
+              info={info}
+              isGraphQL={isGraphQL}
+            />
+            <RequestSchedule
+              handleButtonClick={handleButtonClick}
+              info={info}
+              isGraphQL={isGraphQL}
+            />
+            <h6>API Type</h6>
+            <Switch
+              checkedChildren="GraphQL"
+              unCheckedChildren="REST"
+              defaultChecked
+              onChange={(checked) => setIsGraphQL(checked)}
+            />
+          </div>
+
+          <div style={{ maxWidth: "50%" }}>
+            <h3>Output</h3>
+            {data ? (
+              <pre style={{ fontSize: "12px" }}>
+                {JSON.stringify(data, null, 2)}
+              </pre>
+            ) : null}
+          </div>
+        </div>
       </header>
     </div>
   );
